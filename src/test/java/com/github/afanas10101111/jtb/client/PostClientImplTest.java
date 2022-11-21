392269f985453c74e505485f46aee4608ca8ee1f
Afanas10101111 <68496260+Afanas10101111@users.noreply.github.com>
2022-11-21 22:55:01 +0300

    [JTB-20] Handle NPE in PostClientImpl

--- src/test/java/com/github/afanas10101111/jtb/client/PostClientImplTest.java
+++ src/test/java/com/github/afanas10101111/jtb/client/PostClientImplTest.java
@@ -3,6 +3,7 @@ package com.github.afanas10101111.jtb.client;
 import com.github.afanas10101111.jtb.client.dto.PostInfo;
 import kong.unirest.HttpMethod;
 import kong.unirest.MockClient;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -13,31 +14,39 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class PostClientImplTest {
     private static final String URL = "https://javarush.ru/api/1.0/rest";
+    private static final String POST_PATH = "/posts";
+    private static final String GROUP_KID_PARAM = "groupKid";
+    private static final int LAST_POST_ID = 1;
+    private static final MockClient client = MockClient.register();
 
     private final PostClient postClient = new PostClientImpl(URL);
-
-    private static final String POSTS = "/posts";
-    private static final MockClient client = MockClient.register();
 
     @BeforeAll
     static void mockClientSetup() {
         PostInfo post = new PostInfo();
         post.setId(2);
-        client.expect(HttpMethod.GET, URL + POSTS)
-                .queryString("groupKid", "18")
+        client.expect(HttpMethod.GET, URL + POST_PATH)
+                .queryString(GROUP_KID_PARAM, "18")
                 .thenReturn(Collections.singletonList(post));
 
-        client.expect(HttpMethod.GET, URL + POSTS)
-                .queryString("groupKid", "16")
+        client.expect(HttpMethod.GET, URL + POST_PATH)
+                .queryString(GROUP_KID_PARAM, "16")
                 .thenReturn(List.of());
+
+        client.expect(HttpMethod.GET, URL + POST_PATH)
+                .queryString(GROUP_KID_PARAM, "14")
+                .thenReturn(StringUtils.EMPTY);
     }
 
     @Test
     void findNewPosts() {
-        List<PostInfo> newPosts = postClient.findNewPosts(18, 1);
+        List<PostInfo> newPosts = postClient.findNewPosts(18, LAST_POST_ID);
         assertThat(newPosts).asList().hasSize(1);
 
-        List<PostInfo> outOfNewPosts = postClient.findNewPosts(16, Integer.MAX_VALUE);
+        List<PostInfo> outOfNewPosts = postClient.findNewPosts(16, LAST_POST_ID);
         assertThat(outOfNewPosts).asList().isEmpty();
+
+        List<PostInfo> nullResponse = postClient.findNewPosts(14, LAST_POST_ID);
+        assertThat(nullResponse).asList().isEmpty();
     }
 }
