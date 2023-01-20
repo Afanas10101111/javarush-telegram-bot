417252fd5784b0332a96cd4387f7547e594c8d71
Afanas10101111 <68496260+Afanas10101111@users.noreply.github.com>
2023-01-20 18:58:06 +0300

    [JTB-21] Handle TelegramApiRequestException

--- src/test/java/com/github/afanas10101111/jtb/service/NewArticleServiceImplTest.java
+++ src/test/java/com/github/afanas10101111/jtb/service/NewArticleServiceImplTest.java
@@ -2,18 +2,23 @@ package com.github.afanas10101111.jtb.service;
 
 import com.github.afanas10101111.jtb.client.PostClient;
 import com.github.afanas10101111.jtb.client.dto.PostInfo;
+import com.github.afanas10101111.jtb.exception.BotBlockedByUserException;
 import com.github.afanas10101111.jtb.model.GroupSub;
 import com.github.afanas10101111.jtb.model.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.util.Collections;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
 
 @ExtendWith(MockitoExtension.class)
 class NewArticleServiceImplTest {
@@ -30,29 +35,50 @@ class NewArticleServiceImplTest {
     @InjectMocks
     private NewArticleServiceImpl newArticleService;
 
-    @Test
-    void findAndNotify() {
-        User user = new User();
+    private User user;
+    private GroupSub groupSub;
+    private PostInfo post;
+
+    @BeforeEach
+    void setup() {
+        user = new User();
         user.setChatId("1");
         user.setActive(true);
-        GroupSub groupSub = new GroupSub();
+
+        groupSub = new GroupSub();
         groupSub.setId(1);
         groupSub.setTitle("title");
         groupSub.setLastArticleId(1);
         groupSub.addUser(user);
         Mockito.when(groupSubService.findAll()).thenReturn(Collections.singletonList(groupSub));
 
-        PostInfo post = new PostInfo();
+        post = new PostInfo();
         post.setId(2);
         post.setTitle("postTitle");
         post.setDescription("postDescription");
         post.setKey("postKey");
         Mockito.when(client.findNewPosts(anyInt(), anyInt())).thenReturn(Collections.singletonList(post));
+    }
 
+    @Test
+    void shouldFindAndNotify() {
         newArticleService.findAndNotify();
         Mockito.verify(messageService).sendMessage(user.getChatId(), String.format(
                 NewArticleServiceImpl.MESSAGE_FORMAT,
-                groupSub.getTitle(), post.getTitle(), post.getDescription(), String.format(NewArticleServiceImpl.WEB_POST_FORMAT, post.getKey())
-                ));
+                groupSub.getTitle(),
+                post.getTitle(),
+                post.getDescription(),
+                String.format(NewArticleServiceImpl.WEB_POST_FORMAT, post.getKey())
+        ));
+        assertThat(user.isActive()).isTrue();
+    }
+
+    @Test
+    void shouldMarkAsInactiveUserWhoBlockedTheBot() {
+        Mockito.doThrow(new BotBlockedByUserException(new TelegramApiException()))
+                .when(messageService)
+                .sendMessage(anyString(), anyString());
+        newArticleService.findAndNotify();
+        assertThat(user.isActive()).isFalse();
     }
 }
--- src/test/java/com/github/afanas10101111/jtb/service/SendBotMessageServiceImplTest.java
+++ src/test/java/com/github/afanas10101111/jtb/service/SendBotMessageServiceImplTest.java
@@ -1,6 +1,8 @@
 package com.github.afanas10101111.jtb.service;
 
 import com.github.afanas10101111.jtb.bot.JavaRushTelegramBot;
+import com.github.afanas10101111.jtb.exception.BotBlockedByUserException;
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -9,6 +11,12 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 @ExtendWith(MockitoExtension.class)
 class SendBotMessageServiceImplTest {
@@ -23,12 +31,29 @@ class SendBotMessageServiceImplTest {
 
     @Test
     void shouldProperlySendMessage() throws TelegramApiException {
+        service.sendMessage(CHAT_ID, MESSAGE);
+        Mockito.verify(bot).execute(getSengMessage());
+    }
+
+    @Test
+    void shouldThrowProperExceptionIfTheBotIsBlockedByUser() throws TelegramApiException {
+        Map<String, Object> map = new HashMap<>();
+        map.put("error_code", 0);
+        map.put("description", SendBotMessageServiceImpl.BOT_WAS_BLOCKED_BY_THE_USER);
+        JSONObject jsonObject = new JSONObject(map);
+        TelegramApiRequestException exception = new TelegramApiRequestException("", jsonObject);
+        
+        Mockito.when(bot.execute(getSengMessage())).thenThrow(exception);
+
+        assertThatExceptionOfType(BotBlockedByUserException.class)
+                .isThrownBy(() -> service.sendMessage(CHAT_ID, MESSAGE));
+    }
+
+    private SendMessage getSengMessage() {
         SendMessage sendMessage = new SendMessage();
         sendMessage.setText(MESSAGE);
         sendMessage.setChatId(CHAT_ID);
         sendMessage.enableHtml(true);
-
-        service.sendMessage(CHAT_ID, MESSAGE);
-        Mockito.verify(bot).execute(sendMessage);
+        return sendMessage;
     }
 }
